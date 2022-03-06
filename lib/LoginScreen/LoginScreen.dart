deff40bf0948b256fbe893ab98235671876f8ef5
Richi <clo9450@students.fhv.at>
2022-03-06 20:17:14 +0100

    created temp. ui for the first screen

--- lib/LoginScreen/LoginScreen.dart
+++ lib/LoginScreen/LoginScreen.dart
@@ -9,23 +9,80 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.black12,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text("Herzlich willkommen bei mintag!", style: TextStyle(fontWeight: FontWeight.bold),),
-            ElevatedButton(onPressed: (){
-              final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
-              provider.googleLogin();
-              
-            }, child: const Text("Anmelden"))
-
-          ],
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Expanded(
+                flex: 1,
+                child: SizedBox()
+              ),
+               Expanded(
+                 flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text("Herzlich willkommen bei mintag!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+                      SizedBox(height: 16),
+                      Text("hier kannst du dich mit deinem Google-Account anmelden. Frage hierfür am besten deine Eltern oder Lehrer:In um Hilfe.", textAlign: TextAlign.center, style: TextStyle(color: Color(0xffc4c4c4), fontWeight: FontWeight.bold, fontSize: 14),),
+                    ],
+                  ),
+                )
+              ),
+              Expanded(
+                flex: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Image.asset("assets/img/undraw_back_to_school_inwc.png"),
+                )
+              ),
+              Expanded(
+                flex: 2,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 64),
+                        child: GestureDetector(
+                          onTap: () {
+                             final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
+                              provider.googleLogin();
+                          },
+                          child: Container(
+                            height: 48,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: const Color(0xffaadb12),
+                            ),
+                          
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: const[
+                                 Text("Anmelden", style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white),),
+                              ],
+                            )
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+      
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class MyApp extends StatelessWidget {
           // Notice that the counter didn't reset back to zero; the application
           // is not restarted.
           primarySwatch: Colors.green,
+          backgroundColor: Colors.white
         ),
         home: const HomeScreen(),
       ),
