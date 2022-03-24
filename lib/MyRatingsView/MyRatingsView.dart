3b97800e3e395ae78f9e4a55f024808f93557180
Richi <clo9450@students.fhv.at>
2022-03-24 23:50:55 +0100

    added some routings

--- lib/MyRatingsView/MyRatingsView.dart
+++ lib/MyRatingsView/MyRatingsView.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mintag_application/Database/ModelClasses/UserAccountDTO.dart';
 import 'package:mintag_application/Reusable_Widgets/HeaderContainer.dart';
+import 'package:mintag_application/Reusable_Widgets/Themes.dart';
 
 class MyRatingsView extends StatefulWidget {
 
@@ -16,9 +17,21 @@ class _MyRatingsViewState extends State<MyRatingsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const[
-          HeaderContainer(header: "Meine Bewertungen", subHeader: "Hier siehst die Bewertung aller Einträge, die du bis jetzt gemacht hast.")
+      body: SafeArea(
+        child: Column(
+          children: const[
+            HeaderContainer(header: "Meine Bewertungen", subHeader: "Hier siehst die Bewertung aller Einträge, die du bis jetzt gemacht hast.")
+          ],
+        ),
+      ),
+       bottomNavigationBar: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+              child: ElevatedButton(onPressed: ()=> Navigator.of(context).pop(), child: const Text("Zurück"), style: Themes.secondaryButtonStyle,),
+            ),
+          )
         ],
       ),
     );
--- lib/ThankfulMomentsView/ThankfulMomentsView.dart
+++ lib/ThankfulMomentsView/ThankfulMomentsView.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mintag_application/Database/ModelClasses/UserAccountDTO.dart';
 import 'package:mintag_application/Reusable_Widgets/HeaderContainer.dart';
+import 'package:mintag_application/Reusable_Widgets/Themes.dart';
 
 class ThankfulMomentsView extends StatefulWidget {
 
@@ -16,11 +17,23 @@ class _ThankfulMomentsViewState extends State<ThankfulMomentsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const  [
-          HeaderContainer(header: "Dankbare Momente", subHeader: "Hier werden Situationen angegeben, für die du in letzter Zeit besonders dankbar warst.")
+      body: SafeArea(
+        child: Column(
+          children: const  [
+            HeaderContainer(header: "Dankbare Momente", subHeader: "Hier werden Situationen angegeben, für die du in letzter Zeit besonders dankbar warst.")
+          ],
+        ),
+      ),
+      bottomNavigationBar: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+              child: ElevatedButton(onPressed: ()=> Navigator.of(context).pop(), child: const Text("Zurück"), style: Themes.secondaryButtonStyle,),
+            ),
+          )
         ],
-      )
+      ),
     );
   }
 }
