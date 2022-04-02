869bf209d8dbc8e366f3778389b4996b60b3c171
Richi <clo9450@students.fhv.at>
2022-04-02 20:04:17 +0200

    added a piechart for the my ratings view

--- lib/MyRatingsView/MyRatingsView.dart
+++ lib/MyRatingsView/MyRatingsView.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mintag_application/Database/ModelClasses/UserAccountDTO.dart';
 import 'package:mintag_application/Reusable_Widgets/HeaderContainer.dart';
 import 'package:mintag_application/Reusable_Widgets/Themes.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class MyRatingsView extends StatefulWidget {
 
@@ -14,13 +15,42 @@ class MyRatingsView extends StatefulWidget {
 }
 
 class _MyRatingsViewState extends State<MyRatingsView> {
+
+  Map<String, double>ratingCounts  = {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    ratingCounts["1"] = 0;
+    ratingCounts["2"] = 0;
+    ratingCounts["3"] = 0;
+    ratingCounts["4"] = 0;
+    ratingCounts["5"] = 0;
+
+    for(var entry in widget.userAccountDTO.diary.entries!){
+      for(var entryMsg in entry.entryMsgs){
+        if(!entryMsg.isTextField){
+         String parsedKey = entryMsg.rating.toInt().toString();
+         debugPrint("[--key--]" + parsedKey); 
+         ratingCounts[parsedKey] = ratingCounts[parsedKey]!+1;
+        }
+      }
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: const[
-            HeaderContainer(header: "Meine Bewertungen", subHeader: "Hier siehst die Bewertung aller Einträge, die du bis jetzt gemacht hast.")
+          children: [
+            const HeaderContainer(header: "Meine Bewertungen", subHeader: "Hier siehst die Bewertung aller Einträge, die du bis jetzt gemacht hast."),
+            Expanded(
+              child: PieChart(
+                dataMap: ratingCounts)
+            )
           ],
         ),
       ),
