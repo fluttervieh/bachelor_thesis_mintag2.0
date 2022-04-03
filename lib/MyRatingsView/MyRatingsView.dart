892327ae8697cf5e11af8bc4203fa53ee60230ca
Richi <clo9450@students.fhv.at>
2022-04-03 14:53:11 +0200

    styled the myratings view

--- lib/MyRatingsView/MyRatingsView.dart
+++ lib/MyRatingsView/MyRatingsView.dart
@@ -18,6 +18,65 @@ class _MyRatingsViewState extends State<MyRatingsView> {
 
   Map<String, double>ratingCounts  = {};
 
+  String headerText = "";
+
+  String setHeaderText(){
+    double tmpVal = 0;
+    String tmpKey = "";
+    ratingCounts.forEach((key, value) {
+
+      if(value > tmpVal){
+        tmpKey = key;
+        tmpVal = value;
+      } 
+    });
+
+    switch (tmpKey){
+      case "1":{
+        return "Hmm, dir gehts wohl nicht so gut. Manschmal hilft es, mit jemandem über seine Probleme zu reden.";
+      }
+      case "2":{
+        return "Hmm.. Deine Bewertungen sehen nicht so gut aus..";
+      }
+      case "3":{
+        return "Deine Bewertungen sind ganz schön durchwachsen!";
+      }
+      case "4":{
+        return "Das sieht schon mal ganz gut aus!";
+      }
+      case "5":{
+        return "Hey, dir scheint es überwiegend gut zu gehen. Das ist schön!";
+      }
+      default: {
+        return "Kein Ergebnis";
+      }
+    }
+  }
+
+  final gradientList = <List<Color>>[
+  [
+     Colors.red,
+    Colors.red.withOpacity(0.8),
+  ],
+  [
+     Themes.secondaryColor,
+     Themes.secondaryColor.withOpacity(0.8)
+     
+  ],
+  [
+    Colors.yellow,
+    Colors.yellow.withOpacity(0.8)
+  ],
+  [
+    Colors.greenAccent,
+    Colors.greenAccent.withOpacity(0.8)
+  ],
+  [
+    const Color(0xff0c947b),
+    const Color(0xff0c947b).withOpacity(0.8),
+  ]
+];
+
   @override
   void initState() {
     super.initState();
@@ -37,6 +96,7 @@ class _MyRatingsViewState extends State<MyRatingsView> {
         }
       }
     }
+    headerText = setHeaderText();
   }
 
 
@@ -48,8 +108,45 @@ class _MyRatingsViewState extends State<MyRatingsView> {
           children: [
             const HeaderContainer(header: "Meine Bewertungen", subHeader: "Hier siehst die Bewertung aller Einträge, die du bis jetzt gemacht hast."),
             Expanded(
-              child: PieChart(
-                dataMap: ratingCounts)
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 80,
+                    width: MediaQuery.of(context).size.width,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(headerText, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Color(0xffa4a4a4)),),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(32.0),
+                      child: PieChart(
+                        dataMap: ratingCounts,
+                        animationDuration: const Duration(milliseconds: 1500),
+                        chartType: ChartType.disc,
+                        legendOptions: const LegendOptions(
+                          showLegendsInRow: true,
+                          legendPosition: LegendPosition.bottom,
+                          legendShape: BoxShape.circle,
+                          legendTextStyle:  TextStyle(
+                              fontWeight: FontWeight.bold
+                          )
+                        ),
+                        gradientList: gradientList,
+                   
+                      ),
+                    ),
+                      
+                  ),
+                ],
+              ),
             )
           ],
         ),
